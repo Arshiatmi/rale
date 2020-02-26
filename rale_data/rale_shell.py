f361ee79a2d35b8b257c4ff161e4c033af32a429
root <root@localhost.localdomain>
2020-02-26 13:35:32 -0500

    Add Run System Command To Rale Shell And Improve Something...

--- rale_data/rale_shell.py
+++ rale_data/rale_shell.py
@@ -1,13 +1,27 @@
 #!/usr/bin/python3
+from colorama import Fore,Back, Style
+import os
 
 cmd = ""
 while cmd != "exit":
-    cmd = input("-> ")
+    cmd = input(Fore.GREEN + "{in} :  " + Fore.RESET)
     try:
+        print()
         s = eval(cmd)
         if str(type(s)) != "<class '_sitebuiltins.Quitter'>":
             if s or s != None:
+                print(Fore.RED + "{out} : " + Fore.RESET,end='')
                 print(s)
+                print()
         continue
     except:
-        print("Sorry ! Command Was Not Found !")
+        try:
+            out = os.popen(cmd + " 2> /dev/null").read()
+            out = out.strip()
+            if out:
+            	print(out + "\n")
+            else:
+            	raise
+        except:
+            print(Fore.RED + "Sorry ! Command Was Not Found !" + Fore.RESET)
+            print()
